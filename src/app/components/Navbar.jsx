217445d784691f89469504dc717210bd77a3a2df
ArnabhS <sinhaarnabh888@gmail.com>
2024-06-18 21:49:00 +0530

    setup and navbar added

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,28 @@
+
+"use client";
+
+import {  Navbar } from "flowbite-react";
+import { Button } from "flowbite-react";
+export function Nav() {
+  return (
+    <Navbar fluid rounded>
+      <Navbar.Brand href="/">
+       
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-black">Syntalix</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+      <Button pill>Get Started</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#">About</Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
