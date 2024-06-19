e166fff2118543bf3e9a466a4250abb8c7c9884f
ArnabhS <sinhaarnabh888@gmail.com>
2024-06-19 12:37:15 +0530

    refactor: Update Navbar component to include phone number button

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -10,8 +10,11 @@ export function Nav() {
        
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-black">Syntalix</span>
       </Navbar.Brand>
-      <div className="flex md:order-2">
-      <Button pill>Get Started</Button>
+      <div className="flex md:order-2 gap-2">
+        <a href="tel:+1234567890">
+      <Button pill>Call Us at +9192924949</Button>
+      </a>
+
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
@@ -20,6 +23,7 @@ export function Nav() {
         <Navbar.Link href="#">Services</Navbar.Link>
         <Navbar.Link href="#">Pricing</Navbar.Link>
         <Navbar.Link href="#">Contact</Navbar.Link>
+       
       </Navbar.Collapse>
     </Navbar>
   );
